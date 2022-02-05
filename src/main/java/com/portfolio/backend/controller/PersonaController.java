2fb9a2201e2b0afdcd864434b42a36201335631c
jgromerou <jgromerouro@gmail.com>
2022-02-04 22:37:53 -0300

    agregando @ResponseStatus(HttpStatus.OK)

--- src/main/java/com/portfolio/backend/controller/PersonaController.java
+++ src/main/java/com/portfolio/backend/controller/PersonaController.java
@@ -9,6 +9,7 @@ import com.portfolio.backend.service.IPersonaService;
 import java.util.HashSet;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -30,6 +32,7 @@ public class PersonaController {
     private IPersonaService interPersona;
     
     @GetMapping("/hola")
+    @ResponseStatus(HttpStatus.OK)
     public String decirHola(){
         return "Bienvenido Hola Mundo";
     }
