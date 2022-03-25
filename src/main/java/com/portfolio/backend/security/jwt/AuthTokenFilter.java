2a709195784bf729ea7567ea81b51b4a0917d310
jgromerou <jgromerouro@gmail.com>
2022-03-25 00:11:57 -0300

    Header autorización por Bearer

--- src/main/java/com/portfolio/backend/security/jwt/AuthTokenFilter.java
+++ src/main/java/com/portfolio/backend/security/jwt/AuthTokenFilter.java
@@ -17,6 +17,7 @@ import org.springframework.security.web.authentication.WebAuthenticationDetailsS
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import com.portfolio.backend.security.services.UserDetailsServiceImpl;
+import org.springframework.util.StringUtils;
 
 public class AuthTokenFilter extends OncePerRequestFilter {
   @Autowired
@@ -36,12 +37,11 @@ public class AuthTokenFilter extends OncePerRequestFilter {
         String username = jwtUtils.getUserNameFromJwtToken(jwt);
 
         UserDetails userDetails = userDetailsService.loadUserByUsername(username);
-        
-        UsernamePasswordAuthenticationToken authentication = 
-            new UsernamePasswordAuthenticationToken(userDetails,
-                                                    null,
-                                                    userDetails.getAuthorities());
-        
+        UsernamePasswordAuthenticationToken authentication =
+            new UsernamePasswordAuthenticationToken(
+                userDetails,
+                null,
+                userDetails.getAuthorities());
         authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
 
         SecurityContextHolder.getContext().setAuthentication(authentication);
@@ -54,7 +54,12 @@ public class AuthTokenFilter extends OncePerRequestFilter {
   }
 
   private String parseJwt(HttpServletRequest request) {
-    String jwt = jwtUtils.getJwtFromCookies(request);
-    return jwt;
+    String headerAuth = request.getHeader("Authorization");
+
+    if (StringUtils.hasText(headerAuth) && headerAuth.startsWith("Bearer ")) {
+      return headerAuth.substring(7, headerAuth.length());
+    }
+
+    return null;
   }
 }
