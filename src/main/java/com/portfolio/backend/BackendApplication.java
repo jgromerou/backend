7da712efb52f89418d484e8351f9c989d3d2b6af
jgromerou <jgromerouro@gmail.com>
2022-03-26 19:48:40 -0300

    Alta, ver y descargar una imagen

--- src/main/java/com/portfolio/backend/BackendApplication.java
+++ src/main/java/com/portfolio/backend/BackendApplication.java
@@ -1,13 +1,20 @@
 package com.portfolio.backend;
 
+import com.portfolio.backend.service.FilesStorageService;
+import javax.annotation.Resource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BackendApplication {
-
+    @Resource
+        FilesStorageService storageService;
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
-
+    
+  public void run(String... arg) throws Exception {
+    storageService.deleteAll();
+    storageService.init();
+  }   
 }
