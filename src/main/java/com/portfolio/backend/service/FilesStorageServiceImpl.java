b22d57873d424aa7bd68cf9d2019ae5bb51b1630
jgromerou <jgromerouro@gmail.com>
2022-05-18 21:35:46 -0300

    config application

--- src/main/java/com/portfolio/backend/service/FilesStorageServiceImpl.java
+++ src/main/java/com/portfolio/backend/service/FilesStorageServiceImpl.java
@@ -15,7 +15,7 @@ import org.springframework.util.FileSystemUtils;
 import org.springframework.web.multipart.MultipartFile;
 @Service
 public class FilesStorageServiceImpl implements FilesStorageService {
-  private final Path root = Paths.get("uploads");
+  private final Path root = Paths.get("src/main/java/uploads");
   @Override
   public void init() {
     try {
